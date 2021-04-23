e6ca0de370eaddd528b5c3ec1e9cca7570c028ec
Fariha Fairoz <68825961+nhn09@users.noreply.github.com>
2021-04-23 18:51:21 +0600

    Add files via upload

---
+++ Unnon/interactive.js
@@ -0,0 +1,8 @@
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+
+console.log(firebase);
